9d7a797458ddedd4e45c09bc6f368721ac85a760
EstefaniFelix <estefaniescola1@gmail.com>
2024-06-21 17:36:59 +0000

    salvar

--- app/src/main/java/application/repository/multiRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import application.model.Multiplayer;
-
-public interface multiRepository extends CrudRepository<Multiplayer, Long> {
-    public Iterable<Multiplayer> findByTitulo(String titulo);
-}
